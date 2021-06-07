c7ecd979dd9c9962d63b40808441475ded3ace56
MrVineetPatil <vbpatil9980@gmail.com>
2021-06-07 12:08:57 +0530

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
 
-@app.route("/")
+@app.route('/', methods=['POST'])
 def index():
+    data = request.get_json(force=True)
     secret_key = app.config.get("SECRET_KEY")
-    return f"The configured secret key is {secret_key}."
+    return f"The configured secret key is {data}."
+
+
