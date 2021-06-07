1dd533cefd5eefc962361cf303f65d99a7bc51e5
MrVineetPatil <vbpatil9980@gmail.com>
2021-06-07 12:48:38 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
+import numpy as np
+import pickle
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+model = pickle.load(open('finalized_model.sav', 'rb'))
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
@@ -10,6 +13,15 @@ app.config.from_object(env_config)
 def index():
     data = request.get_json(force=True)
     secret_key = app.config.get("SECRET_KEY")
-    return f"The configured secret key is {data}."
+    prediction = model.predict([np.array([data['Contact with confirmed'],
+                                          data['Headache'],
+                                          data['Sore throat'],
+                                          data['Shortness of breath'],
+                                          data['Cough'],
+                                          data['Fever'],
+                                          data['Male'],
+                                          data['Age 60+']])])
+    output = prediction[0]
+    return f"The configured secret key is {output}."
 
 
