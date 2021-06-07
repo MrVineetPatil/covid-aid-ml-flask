e20c6e3dc13d74ec7cc04e09f8faa934ab095866
MrVineetPatil <vbpatil9980@gmail.com>
2021-06-07 13:29:39 +0530

    first commit

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def index():
         output =float(prediction[0])*100
     else:
         output =float(prediction[0])*300
-    return f"The probability of being COVID +VE is {output}."
+    return f"The probability of being COVID +VE is {output}%"
 
 
